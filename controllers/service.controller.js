d12cd68dd97c35d4088735c4863453d745b75c2b
Tijil Agrawal <77985046+Tijil2111@users.noreply.github.com>
2026-08-25 16:13:12 +0530

    room order system completed

--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -1,26 +1,25 @@
 import Booking from "../models/booking.model.js";
 import Service from "../models/service.model.js";
+import Menu from "../models/menuItem.model.js";
 export const createOrder = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const { menuItems, status } = req.body;
+    const { menuItems } = req.body;
     const today = new Date();
-    const isoToday = today.toISOString().split("T")[0];
 
-    const booking = await Booking.findById({ id });
+    const booking = await Booking.findById(id);
     if (!booking) {
-      return res.status(400).json({
+      return res.status(404).json({
         success: false,
         message: "booking doesnt exist",
       });
     }
     const checkIn = booking.check_in;
     const checkOut = booking.check_out;
-    if (checkIn <= isoToday && checkOut >= isoToday) {
+    if (checkIn <= today && checkOut >= today) {
       const newOrder = await Service.create({
         bookingId: id,
         menuItems: menuItems,
-        status: status,
       });
       return res.status(200).json({
         success: true,
@@ -28,7 +27,69 @@ export const createOrder = async (req, res, next) => {
         data: newOrder,
       });
     } else {
+      return res.status(400).json({
+        success: false,
+        message: "you dont have a booking today",
+      });
     }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getRoomOrder = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const services = await Service.find({ bookingId: id });
+    if (!services) {
+      return res.status(400).json({
+        success: false,
+        message: "no services found for this booking id/id is invalid",
+      });
+    }
+    let totalBill = 0;
+    for (const service of services) {
+      for (const item of service.menuItems) {
+        const menuItem = await Menu.findOne({ name: itemName });
+        const itemTotal = menuItem.price * item.qty;
+        totalBill += itemTotal;
+      }
+    }
+    return res.status(200).json({
+      success: true,
+      message: "service data gotten",
+      data: services,
+      totalBill: totalBill,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateOrder = async (req, res, next) => {
+  try {
+    const { status } = req.body;
+    const { id } = req.params;
+    const service = await Service.findById(id);
+    if (!service) {
+      return res.status(400).json({
+        success: false,
+        message: "no services found for this booking id/id is invalid",
+      });
+    }
+    const updatedService = await Service.findByIdAndUpdate(
+      { id },
+      { status: status },
+      {
+        returnDocument: "after",
+        runValidators: true,
+      },
+    );
+    return res.status(200).json({
+      success: true,
+      message: "status of service updated succesfully",
+      data: updatedService,
+    });
   } catch (error) {
     next(error);
   }
--- routes/booking.router.js
+++ routes/booking.router.js
@@ -3,10 +3,18 @@ import {
   createBooking,
   findBooking,
 } from "../controllers/booking.controller.js";
+import {
+  createOrder,
+  getRoomOrder,
+  updateOrder,
+} from "../controllers/service.controller.js";
 
 const bookingRouter = express.Router();
 
 bookingRouter.get("/", findBooking);
 bookingRouter.post("/", createBooking);
+bookingRouter.post("/:id/room-service", createOrder);
+bookingRouter.get("/:id/room-service", getRoomOrder);
+bookingRouter.patch("/room-service/:id", updateOrder);
 
 export default bookingRouter;
